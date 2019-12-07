4ab332c2f2df176497d645d1fa64661cd5d4e4ac
Sai Sankeerth <sankeerth2292@gmail.com>
2019-12-07 11:36:50 +0530

    Split alternating elements in two LinkedLists sol commit

--- linkedlist/linkedlist-prob2.py
+++ linkedlist/linkedlist-prob2.py
@@ -1,6 +1,41 @@
 from linkedlist import LinkedList, convertToLinkedList, Node
 
 
+def split_into_two_lists(l: LinkedList):
+    '''
+    Split given linked list into two lists where each list 
+    containing alternating elements from it
+
+    '''
+    i = 0
+    node = l.head
+    list1 = LinkedList()
+    list2 = LinkedList()
+    h1 = list1.head
+    h2 = list2.head
+    while(node):
+        new_node = Node(node.data)
+        if i % 2 == 0:
+            if h1:
+                h1.next = new_node
+            else:
+                list1.head = new_node
+                h1 = list1.head
+            if h1.next:
+                h1 = h1.next
+        else:
+            if h2:
+                h2.next = new_node
+            else:
+                list2.head = new_node
+                h2 = list2.head
+            if h2.next:
+                h2 = h2.next
+        node = node.next
+        i += 1
+    return list1, list2
+
+
 def split_mid(l: LinkedList):
     node = l.head
     if not node:
@@ -32,5 +67,10 @@ def split_mid(l: LinkedList):
 
 if __name__ == "__main__":
     ll = convertToLinkedList([1, 2, 3, 4, 5])
-    d = split_mid(ll)
-    print(d)
+    # d = split_mid(ll)
+    # print(d)
+    d = split_into_two_lists(ll)
+    print('First List')
+    d[0].printAll()
+    print('Second List')
+    d[1].printAll()
