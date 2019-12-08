bc4cd7a37f72a120be4b3e5a8c6a51f4f6b37223
Sai Sankeerth <sankeerth2292@gmail.com>
2019-12-08 19:28:41 +0530

    Merge Alternate Nodes problem solution commit

--- linkedlist/linkedlist-prob2.py
+++ linkedlist/linkedlist-prob2.py
@@ -1,5 +1,68 @@
 from linkedlist import LinkedList, convertToLinkedList, Node
 
+'''
+1) Find K’th node from the end in a linked list
+2) Merge alternate nodes of two linked lists into the first list
+'''
+
+
+def mergeAlternateNodes(l1: LinkedList, l2: LinkedList):
+    '''
+        Merge Alternate nodes of two linked lists into the first list
+        l1 = {1,2,3}
+        l2 = {4,5,6,7,8}
+
+        Result:
+        l1 = {1,4,2,5,3,6}
+        l2 = {7,8}
+    '''
+    n1 = l1.head
+    n2 = l2.head
+    while(n1 and n2):
+        # if i % 2 != 0:
+        tmp = n2.next
+        new_node = Node(n2.data)
+        new_node.next = n1.next
+        n1.next = new_node
+        n2 = tmp
+        n1 = n1.next.next
+    # Change the head to the last Node that is present after above Iteration
+    l2.head = n2
+    l1.printAll()
+    l2.printAll()
+
+
+def findKthNodeFromEnd(l: LinkedList, k: int):
+    '''
+        Find Kth node from the end in a LinkedList
+    '''
+    node = l.head
+    if k <= 0:
+        print('The value of k should be > 0')
+        return
+    if not node:
+        print('Cannot Find requested Element from Empty LinkedList')
+        return
+    memo = {}
+    ind = 1
+    if node.next:
+        while(node):
+            memo[ind] = node.data
+            if node.next:
+                ind += 1
+            node = node.next
+        if k > ind:
+            print('No Element exists, since k exceeds length of LinkedList')
+            return
+        else:
+            return memo.get(ind - k + 1)
+    else:
+        if k > 1:
+            print('Only one node exists in LinkedList & k > 1')
+            return
+        else:
+            return node.data
+
 
 def moveLastNodeToFront(l: LinkedList):
     node = l.head
@@ -82,7 +145,9 @@ def split_mid(l: LinkedList):
 
 
 if __name__ == "__main__":
-    ll = convertToLinkedList([1, 2, 3, 4, 5])
+    # ll = convertToLinkedList([1, 2, 3, 4, 5])
+    # ll = convertToLinkedList([1, 8, 3, 2, 4, 6, 7, 5, 9])
+    # ll = convertToLinkedList([1])
     # d = split_mid(ll)
     # print(d)
     # d = split_into_two_lists(ll)
@@ -90,4 +155,9 @@ if __name__ == "__main__":
     # d[0].printAll()
     # print('Second List')
     # d[1].printAll()
-    moveLastNodeToFront(ll)
+    # moveLastNodeToFront(ll)
+    # kth = findKthNodeFromEnd(ll, 5)
+    # print(kth)
+    l1 = convertToLinkedList(list(range(1, 4, 1)))
+    l2 = convertToLinkedList(list(range(4, 9, 1)))
+    mergeAlternateNodes(l1, l2)
